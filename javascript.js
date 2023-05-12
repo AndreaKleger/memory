10d7d4513c692d73a3ecfb93d0e6674010f32af4
AndreaKleger <111911998+AndreaKleger@users.noreply.github.com>
2023-05-12 14:24:32 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+let cards = [];
+cards.push("./resources/ape.png");
+cards.push("./resources/ape.png");
+cards.push("./resources/elephant.png");
+cards.push("./resources/elephant.png");
+
+let playingCards = [];
+
+function randomNumber(max) {
+  return Math.round(Math.random() * max);
+}
+
+function resetCards() {
+  let count = 0;
+  playingCards = [];
+  let localCards = cards.slice();
+  while (localCards.length > 0) {
+    count++;
+    let randomNr = randomNumber(localCards.length - 1);
+    console.log(randomNr);
+
+    let path = localCards[randomNr];
+    console.log(path);
+    // wir fügen die Pfade jetzt der Reihe nach in den Array ein,
+    // aus dem wir später während dem Spiel die Karten beziehen
+    playingCards.push(path);
+
+    //jetzt fügen wir auf dem Feld die Kartenrückseiten ein
+    document.getElementById("card" + count).src = "./resources/background.png";
+    //der bereits gebrauchte Pfad wird aus dem Array entfernt
+    localCards.splice(randomNr, 1);
+  }
+}
+
+function turnCard(cardNr) {
+  console.log(playingCards);
+  document.getElementById("card" + cardNr).src = playingCards[cardNr - 1];
+}
